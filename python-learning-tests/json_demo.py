480a367d5bcaf5a7523e032563f4588df78b49a9
LucasSlepetys <134546411+LucasSlepetys@users.noreply.github.com>
2023-05-30 14:44:16 +0200

    Update json_demo.py

--- python-learning-tests/json_demo.py
+++ python-learning-tests/json_demo.py
@@ -5,13 +5,13 @@ people_string = '''
     "people": [
         {
             "name": "Lucas Slepetys",
-            "phone": "615-555-7164",
+            "phone": "615-555",
             "emails": ["lucas.slepetys@gmail.com", "lucas.slepetyss@hotmail.com"],
             "has_licence": false
         },
         {
-            "name": "John Rodrigues",
-            "phone": "111-789-1111",
+            "name": "John ",
+            "phone": "111-789-",
             "emails": null,
             "has_licence": true
         }
